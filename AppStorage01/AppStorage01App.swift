15c82e67921f08724f3c80be7ece00a4ba9b9fe4
R. Kukuh <rkukuh@gmail.com>
2023-05-12 10:07:58 +0700

    Initial Commit

---
+++ AppStorage01/AppStorage01App.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorage01App.swift
+//  AppStorage01
+//
+//  Created by R. Kukuh on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorage01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
